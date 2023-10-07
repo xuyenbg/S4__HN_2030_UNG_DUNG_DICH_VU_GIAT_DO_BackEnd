ccc4fd96b73878ec752798491dd1cc0c5adb2f05
luannt2809 <thanhluan2818@gmail.com>
2023-10-07 23:35:07 +0700

    config db, demo home page

---
+++ configs/db_config.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+function connectDB() {
+  const url =
+    "mongodb+srv://washnowdb:washnowdb@washnowcluster.5qeq2qb.mongodb.net/?retryWrites=true&w=majority/washnowdb";
+
+  try {
+    mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  const dbConnection = mongoose.connection;
+  dbConnection.once("open", (_) => {
+    console.log(`Database connected`);
+  });
+
+  dbConnection.on("error", (err) => {
+    console.error(`Connection error: ${err}`);
+  });
+
+  return;
+}
+
+module.exports = connectDB();
