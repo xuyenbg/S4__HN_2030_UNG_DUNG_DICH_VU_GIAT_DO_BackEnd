a80f3088dbf5a94ae506117d70adb123cdf04525
luannt2809 <thanhluan2818@gmail.com>
2023-10-08 14:45:49 +0700

    tao model roles

--- configs/db_config.js
+++ configs/db_config.js
@@ -1,29 +1,44 @@
+// const mongoose = require("mongoose");
+
+// function connectDB() {
+//   const url =
+//     "mongodb+srv://washnowdb:washnowdb@washnowcluster.5qeq2qb.mongodb.net/?retryWrites=true&w=majority/washnowdb";
+
+//   try {
+//     mongoose.connect(url, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//   } catch (err) {
+//     console.error(err);
+//     process.exit(1);
+//   }
+
+//   const dbConnection = mongoose.connection;
+//   dbConnection.once("open", (_) => {
+//     console.log(`Database connected`);
+//   });
+
+//   dbConnection.on("error", (err) => {
+//     console.error(`Connection error: ${err}`);
+//   });
+
+//   return;
+// }
+
+// module.exports = connectDB();
+
 const mongoose = require("mongoose");
 
-function connectDB() {
-  const url =
-    "mongodb+srv://washnowdb:washnowdb@washnowcluster.5qeq2qb.mongodb.net/?retryWrites=true&w=majority/washnowdb";
-
-  try {
-    mongoose.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-
-  const dbConnection = mongoose.connection;
-  dbConnection.once("open", (_) => {
-    console.log(`Database connected`);
+mongoose
+  .connect(
+    "mongodb+srv://washnowdb:washnowdb@washnowcluster.5qeq2qb.mongodb.net/washnowdb?retryWrites=true&w=majority"
+  )
+  .then((_) => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
   });
 
-  dbConnection.on("error", (err) => {
-    console.error(`Connection error: ${err}`);
-  });
-
-  return;
-}
-
-module.exports = connectDB();
+module.exports = mongoose;
