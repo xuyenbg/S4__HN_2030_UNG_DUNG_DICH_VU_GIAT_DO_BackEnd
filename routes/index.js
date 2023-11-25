b357b773ab0021225cf8e3d6a9020f697c5d29dd
luanntph26376 <luannt113@fpt.com>
2023-11-25 09:36:34 +0700

    add table web server

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,24 @@ var express = require("express");
 var router = express.Router();
 const checkLogin = require("../middleware/checkLogin");
 const AuthController = require("../controllers/auth_controller");
-const HomeCtl = require('../controllers/home_controler');
+const HomeCtl = require("../controllers/home_controler");
 // /* GET home page. */
 // // router.get("/", checkLogin.reqLogin, function (req, res, next) {
 // //   res.render("home", { title: "Express" });
 // // });
 
-router.get("/",HomeCtl.data);
+router.get("/", HomeCtl.data);
 
 router.get("/logout", checkLogin.reqLogin, AuthController.logoutAdmin);
+
+// đoạn code ở đây chỉ mang tính chất hiện lên trang table
+router.get("/table", async (req, res) => {
+  try {
+    res.render("table");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Có lỗi xảy ra");
+  }
+});
 
 module.exports = router;
