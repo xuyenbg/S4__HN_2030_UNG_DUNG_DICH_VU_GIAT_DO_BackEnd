f88d3ad7639e3b257e450b8992b2db7a2b141341
las-longkp <139189344+las-longkp@users.noreply.github.com>
2023-12-02 03:51:54 +0700

    thong_ke

--- routes/stores_router.js
+++ routes/stores_router.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const StoreController = require('../controllers/store_controller')
+const checkLogin = require("../middleware/checkLogin");
 
-router.get('/list', StoreController.getListStore)
+
+router.get('/list',checkLogin.reqLogin, StoreController.getListStore)
 
 module.exports = router
