1a7b108f6bc1dcd6284bf39b9ccc1335e0a9f508
las-longkp <139189344+las-longkp@users.noreply.github.com>
2023-12-01 23:05:51 +0700

    fix_thong_ke

--- routes/order_router.js
+++ routes/order_router.js
@@ -1,9 +1,15 @@
 var express = require('express');
-const { getListOrderModel, getlistOrderByDate } = require('../controllers/order_controler');
+const { getListOrderModel, getlistOrderByDate, getlistOrderByWeek, getListTotalOrder, getlistOrderByMonth, getlistOrderByYear } = require('../controllers/order_controler');
 var router = express.Router();
 
 router.get('/list',getListOrderModel);
+router.get('/listTotalOrder',getListTotalOrder)
 router.get('/listOrderByDate',getlistOrderByDate)
+router.get('/listOrderByWeek',getlistOrderByWeek)
+router.get('/listOrderByMonth',getlistOrderByMonth)
+router.get('/listOrderByYear',getlistOrderByYear)
+
+
 
 
 module.exports = router;
